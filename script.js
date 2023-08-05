3846e1e23378994cfa95e15e754e8fad6a70a2da
ReeperC3 <cian3neiland@gmail.com>
2023-08-05 08:19:14 +0100

    Made it so you can change the size of grid and it changes color when you click the squares.

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const container = document.querySelector(".container");
+let defaultSize = 16;
+let currentMode = 'color';
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 function createGrid(rows, columns) {
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < columns; j++){
     let square = document.createElement('div');
     square.className = 'square';
-    square.addEventListener('mouseover', changeColor);
+    square.addEventListener('mouseover', changeColor)
+    square.addEventListener('mousedown', changeColor)
     container.appendChild(square);
   }}}
 
-  function changeColor() {
-    this.style.backgroundColor = "blue";
+  function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown ) return;
+    if (currentMode === 'color') {
+      e.target.style.backgroundColor = "blue";
+    }
+    
   }
 
-createGrid(16, 16);
+  function gridSize(size = 16) {
+    if (size <4 || size > 100) {
+      console.log("ERROR - size too big or small") 
+    } else {
+      createGrid(size, size);
+      container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+      container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    }
+    
+  }
+window.onload = gridSize(defaultSize)
+
+
+const changeSize = document.querySelector(".change-size");
+changeSize.onchange = (e) => {gridSize(e.target.value)}
+
