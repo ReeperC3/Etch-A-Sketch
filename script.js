d89cc85ab10d27b5eea9e88c6245f70ae2daca59
ReeperC3 <cian3neiland@gmail.com>
2023-08-05 17:46:32 +0100

    Added rainbow feature, rubber, clear and tidied up page.

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const container = document.querySelector(".container");
 let defaultSize = 16;
-let currentMode = 'color';
+let defaultMode = 'color';
+let currentMode = defaultMode;
+let defaultColor = 'black';
+const rainbow = document.querySelector('.rainbow');
+const colorPicker = document.querySelector('.color');
+const colorBtn = document.querySelector('.color-btn');
+const rubber = document.querySelector('.rubber');
+const clear = document.querySelector('.clear');
 let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+let color = defaultColor;
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+rainbow.onclick = () => setCurrentMode('rainbow');
+colorBtn.onclick = () => setCurrentMode('color');
+rubber.onclick = () => setCurrentMode('rubber');
+clear.onclick = () => clearGrid();
+
+function clearGrid() {
+  container.innerHTML = '';
+  gridSize(defaultSize);
+}
+
+function setCurrentMode(newMode) {
+  currentMode = newMode;
+}
+
 function createGrid(rows, columns) {
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < columns; j++){
@@ -14,11 +38,22 @@ for (let i = 0; i < rows; i++) {
     container.appendChild(square);
   }}}
 
+  function setCurrentColor(newColor) {
+    color = newColor;
+  }
+
   function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown ) return;
     if (currentMode === 'color') {
-      e.target.style.backgroundColor = "blue";
-    }
+      e.target.style.backgroundColor = color;
+    } else if (currentMode === 'rainbow') {
+      let randomR = Math.floor(Math.random() * 256);
+      let randomG = Math.floor(Math.random() * 256);
+      let randomB = Math.floor(Math.random() * 256);
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'rubber') {
+      e.target.style.backgroundColor = 'white';
+    } 
     
   }
 
@@ -32,6 +67,7 @@ for (let i = 0; i < rows; i++) {
     }
     
   }
+
 window.onload = gridSize(defaultSize)
 
 
